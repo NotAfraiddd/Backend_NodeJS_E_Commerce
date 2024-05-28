1adc18158f58f997c5fbd602cd936f415908878a
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2024-05-28 11:17:07 +0700

    fix: edit route image upload

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const cors = require('cors')
 const port = process.env.PORT || 4000
 
 const productRouter = require('./routes/product')
+const imageUploadRouter = require('./upload/imageUpload')
 
 app.use(express.json())
 app.use(cors())
@@ -17,32 +18,12 @@ app.use(cors())
 // routes
 app.use('/api/product', productRouter)
 
+// route upload image
+app.use('/api/image', imageUploadRouter)
+
 const link_mongoose = process.env.LINK_MONGOOSE
 // Database connect mongoDB
 mongoose.connect(link_mongoose)
-
-// API
-app.get('/', (req, res) => {
-  res.send('App is running')
-})
-
-// image storage
-const storage = multer.diskStorage({
-  destination: './upload/images',
-  filename: (req, res, cb) => {
-    return cb(null, `${res.fieldname}_${Date.now()}${path.extname(res.originalname)}`)
-  }
-})
-
-const upload = multer({ storage: storage })
-
-app.use('/images', express.static('upload/images'))
-app.post('/upload', upload.single('product'), (req, res) => {
-  res.json({
-    success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`
-  })
-})
 
 // connect server
 app.listen(port, (error) => {
---
+++ upload/imageUpload.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const port = process.env.PORT || 4000
+
+const storage = multer.diskStorage({
+  destination: './upload/images',
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+const router = express.Router()
+
+router.use('/images', express.static('upload/images'))
+
+router.post('/upload', upload.single('product'), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`
+  })
+})
+
+module.exports = router

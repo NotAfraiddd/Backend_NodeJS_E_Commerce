b0daafdab58538af645ace61d14aecde825825a9
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2024-06-11 09:37:09 +0700

    feat: add logic refresh token

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,11 +28,38 @@ module.exports = {
           id: user.id
         }
       }
-      const token = jwt.sign(data, 'secret_ecom', { expiresIn: '1h' })
+      const accesstoken = jwt.sign(data, 'secret_ecom', { expiresIn: '300' })
+      const refreshToken = jwt.sign({ email: req.body.email }, 'refresh_secret_ecom', { expiresIn: '1h' })
 
-      res.json({ success: true, token })
+      res.json({ success: true, accesstoken, refreshToken })
     } catch (error) {
       console.error('Signup error:', error)
+      res.status(500).json({ success: false, error: 'Internal Server Error' })
+    }
+  },
+  refreshToken: async (req, res) => {
+    try {
+      const { refreshToken } = req.body
+      if (!refreshToken) {
+        return res.status(401).json({ success: false, error: 'Refresh token is required' })
+      }
+
+      const user = await User.findOne({ refreshToken })
+      if (!user) {
+        return res.status(403).json({ success: false, error: 'Invalid refresh token' })
+      }
+
+      // Xác thực refresh token
+      jwt.verify(refreshToken, 'refresh_secret_ecom', (err, decoded) => {
+        if (err) {
+          return res.status(403).json({ success: false, error: 'Invalid refresh token' })
+        }
+
+        const accessToken = jwt.sign({ user: { id: user.id } }, 'secret_ecom', { expiresIn: '15m' })
+        res.json({ success: true, accessToken })
+      })
+    } catch (error) {
+      console.error('Refresh token error:', error)
       res.status(500).json({ success: false, error: 'Internal Server Error' })
     }
   }
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const auth = require('../controllers/auth')
 
 router.post('/signup', auth.signup)
+router.post('/refresh-token', authController.refreshToken)
 
 module.exports = router
